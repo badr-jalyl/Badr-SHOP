27f37c3f27a0c798ccaeae692cf0b8862569a144
badr-jalyl <82679833+badr-jalyl@users.noreply.github.com>
2021-04-16 22:15:24 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+/** function header **/
+function stickyMenu() {
+	var sticky=document.getElelementById('sticky');
+		if(window.pageYOffsetById > 220) {
+			sticky.classList.add('sticky');
+		}
+		else{
+			sticky.classList.remove('sticky');
+		}
+	}
+	window.onscroll =  function(){
+		stickyMenu();
+	}
+	
+/** function slider **/
+	var slides=document.querySelector('.slider').children;
+	var prevSlide=document.querySelector(".prev");
+	var nextSlide=document.querySelector(".next");
+	var totalSlides=slides.length;
+	var index=0;
+
+	prevSlide.onclick=function () {
+		next("prev");
+	}
+	nextSlide.onclick=function () {
+		next("next");
+	}
+
+	function next(direction){
+		if(direction=="next"){
+			index++;
+			if(index==totalSlides){
+				index=0;
+			}
+		}
+		else {
+			if(index==0){
+				index=totalSlides-1;
+			}
+			else{
+				index--;
+			}
+		}
+		for(i=0;i<slides.length;i++){
+			slides[i].classList.remove("active")	
+		}
+		slides[index].classList.add("active");
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
